d6081fd97f0b2e968e27c56a2431c2cdc282ecac
olgakycenko <olgakucenko97@gmail.com>
2025-07-03 17:43:39 +0300

    Update Telegram bot token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Telegram Bot Token
-TOKEN = "7964302886:AAH8LCh4qEqZ_7sTxfWuWlPKRP9-B3FPw68"
+TOKEN = "7554486624:AAF0bXfgljKuJ7I9fuMrGNx2dOD9Jnhl8Zc"
 
 # Admin ID (your Telegram ID)
 ADMIN_ID = 5231490758
